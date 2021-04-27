a7a09918eca48caa3d4452d2f772bd5e23635912
Mattias Karlsson <mattias.karlsson13@learnet.se>
2021-04-27 16:58:29 +0200

    Add project files.

---
+++ EmailAPI/Controllers/ContactController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EmailAPI.Controllers
+{
+   
+    public class ContactController : ControllerBase
+    {
+      
+
+        [HttpPost("sendcontact")]
+        public async Task<IActionResult> SendContact([FromBody] Contact contact)
+        {
+            try
+            {
+                using (SmtpClient client = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    client.EnableSsl = true;
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.UseDefaultCredentials = false;
+                    client.Credentials = new NetworkCredential("kenkataservice@gmail.com", "bxieecxvpcowbnsl");
+
+                    MailMessage message = new MailMessage();
+                    message.To.Add("kenkataservice@gmail.com");
+                    message.From = new MailAddress(contact.Email);
+                    message.Subject = (contact.Subject);
+                    message.Body = ($"From: {contact.Name}\n Email: {contact.Email}\n Message: {contact.Message}");
+                    client.Send(message);
+
+                }
+            }
+            catch
+            {
+                return new BadRequestResult();
+            }
+
+            return new OkResult();
+        }
+    }
+}
---
+++ EmailAPI/Controllers/EmailController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+using System.Net;
+using System.Net.Mail;
+
+
+namespace EmailAPI.Controllers
+{
+    public class EmailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("sendemail")]
+        public async Task<IActionResult> SendEmail([FromBody] Email email)
+        {
+            try
+            {
+              
+                    using(SmtpClient client = new SmtpClient("smtp.gmail.com", 587))
+                    {
+                        client.EnableSsl = true;
+                        client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                        client.UseDefaultCredentials = false;
+                        client.Credentials = new NetworkCredential("kenkataservice@gmail.com", "Password");
+
+                        MailMessage message = new MailMessage();
+                        message.To.Add(email.To);
+                        message.From = new MailAddress("Mattias@outlook.com");
+                        message.Subject = "Welcome New Subscriber!!";
+                        message.Body = "It also features quite detailed e-mail tracking";
+                        client.Send(message);
+
+
+                        MailMessage mess = new MailMessage();
+                        mess.To.Add("kenkataservice@gmail.com");
+                        mess.From = new MailAddress("Mattias@outlook.com");
+                        mess.Subject = "A New Subscriber!!";
+                        mess.Body = $"{email.To} is now subscribing!";
+                        client.Send(mess);
+
+
+
+
+                       
+
+                    }
+            }
+            catch {
+                return new BadRequestResult();
+            }
+
+            return new OkResult();
+        }
+    }
+}
